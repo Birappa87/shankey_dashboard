fdf52865ccfaee8f173e9ad11f0f8b56544fd940
Birappa87 <birappa.001@gmail.com>
2025-11-11 08:35:55 +0530

    commit

--- app.py
+++ app.py
@@ -186,8 +186,8 @@ def load_data():
 def display_header():
     """Displays the application header with logo and organization name."""
     col1, col2 = st.columns([1, 5])
-    with col1:
-        st.image("logo.png", width=100)
+    # with col1:
+    #     st.image("logo.png", width=100)
     with col2:
         st.markdown("""
             <div style='padding-top:15px;'>
